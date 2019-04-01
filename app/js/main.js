d43d0de83bf914634b97b4130a80315bbf26c318
DOKHELL <cerge.gladkovv@gmail.com>
2019-04-01 11:47:43 +0300

    all block

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+
+	$('.header__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',});
+    });
